43c920fa169b96c8d5d329a8083730092da9b584
Tomonobu <fukchr@gmail.com>
2022-12-29 17:31:24 -0500

    TodoList App - react

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
+import Task from './components/Task';
+import Input from './components/Input';
+import { useState, useEffect } from 'react'
+
 
 function App() {
+
+  const [ tasks, setTasks ] = useState([]);
+
+  useEffect(() => {
+    const recoverTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (recoverTasks.length) {
+      setTasks(recoverTasks);
+    }
+  }, []);
+  
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  const onSubmit = newTask => {
+    if (newTask.text.trim()){
+      newTask.text = newTask.text.trim();
+      const newTasks = [ newTask, ...tasks];
+      setTasks(newTasks);
+    }
+  };
+
+  const toggleCompleted = id => {
+    const newTasks = tasks.map(task => {
+      if (task.id === id) task.completed = !task.completed;
+      return task;
+    });
+    setTasks(newTasks);
+  };
+
+  const deleteTask = id => {
+    const newTasks = tasks.filter(task => task.id !== id);
+    setTasks(newTasks)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='todoList-app'>
+        <div className='todoList-container'>
+          <h1>My Tasks</h1>
+          <Input onSubmit={onSubmit}/>
+          { 
+            tasks.map( task => 
+              <Task 
+                key={task.id}
+                id={task.id}
+                text={task.text} 
+                completed={task.completed}
+                toggleCompleted = {toggleCompleted}
+                deleteTask = {deleteTask} />) 
+          }
+        </div>
+      </div>
     </div>
   );
 }
